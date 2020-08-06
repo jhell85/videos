f03639296bfd0a22db3501c4dce2ea1761b52b91
jhell85 <jhell85@gmail.com>
2020-08-06 08:28:44 -0700

    add video detail component when someone clicks on a video

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import videoList from "./VideoList";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -12,14 +13,21 @@ class App extends React.Component {
         q: term,
       },
     });
-
     this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
