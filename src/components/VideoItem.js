0ed449a4c1fa44f0864706d0029de7506f12c4bb
jhell85 <jhell85@gmail.com>
2020-08-05 22:24:16 -0700

    add some styling for when videos are rendered

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from "react";
-
-const VideoItem = () => {
-  return <div>VideoItem</div>;
+import "./VideoItem.css";
+const VideoItem = ({ video }) => {
+  return (
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
